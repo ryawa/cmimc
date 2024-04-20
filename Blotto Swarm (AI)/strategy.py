93ac4a601332c98e1fad0cd8d01e736647869002
Ryan Wang <rw07041+Github@gmail.com>
2024-04-20 13:49:43 -0400

    a

--- Blotto Swarm (AI)/strategy.py
+++ Blotto Swarm (AI)/strategy.py
@@ -3,6 +3,7 @@ Edit this file! This is the file you will submit.
 """
 
 import random
+import sys
 
 """
 NOTE: Each soldier's memory in the final runner will be separate from the others.
@@ -10,27 +11,33 @@ NOTE: Each soldier's memory in the final runner will be separate from the others
 WARNING: Do not print anything to stdout. It will break the grading script!
 """
 
-def strategy(ally: list, enemy: list, offset: int) -> int:
+def ryan(ally: list, enemy: list, offset: int) -> int:
+    if offset == 0 and enemy[3] < 7:
+        return 0
+    return 1
+
+def roger(ally: list, enemy: list, offset: int) -> int:
     # If we are at a castle
+    castle_neighbors = [2, 3, 4]
+    castle_neighbors = [x + offset for x in castle_neighbors]
+    num_allies = sum([ally[i] for i in castle_neighbors])
+    num_enemies = sum([enemy[i] for i in castle_neighbors])
     if offset == 0:
-        if ally[3] > 10:
+        if ally[3] > 7:
             return random.choice([-1, 1])
     elif offset == -1:
-        num_enemies = enemy[2] + enemy [3] + enemy [1]
-        if num_enemies > 15:
+        # print(castle_neighbors, file=sys.stderr)
+        if num_enemies > 7:
             return -offset
         return offset
     elif offset == 1:
-        num_enemies = enemy[3] + enemy[4] + enemy[5]
-        if num_enemies > 15:
+        if num_enemies > 7:
             return -offset
         return offset
-    # ttest
  
-def random_strategy(ally: list, enemy: list, offset: int) -> int:
+def wenhao(ally: list, enemy: list, offset: int) -> int:
     # A simple strategy
-    return random.choice([-1, 0, 1])
-
+    return offset
 
 def get_strategies():
     """
@@ -41,6 +48,6 @@ def get_strategies():
 
     In the official grader, only the first element of the list will be used as your strategy.
     """
-    strategies = [strategy, random_strategy]
+    strategies = [wenhao, roger, ryan]
 
     return strategies
